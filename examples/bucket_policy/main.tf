27e1a17e22d773e861639f92209c0df913ab7a2b
Quentin Vallin <quentin.vallin@fxinnovation.com>
2019-11-18 17:36:29 -0500

    fix/ data source

--- examples/bucket_policy/main.tf
+++ examples/bucket_policy/main.tf
@@ -50,8 +50,7 @@ data "aws_iam_policy_document" "s3" {
     ]
 
     resources = [
-      "${element(concat(aws_s3_bucket.this.*.arn, list("")), 0)}",
-      "${element(concat(aws_s3_bucket.this.*.arn, list("")), 0)}/*",
+      "${module.standard.arn}",
     ]
   }
 }
